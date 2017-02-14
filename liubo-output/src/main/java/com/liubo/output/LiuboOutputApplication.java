8e9af334a53b264b7fa22d9870429715617672a7
liubo <917551811@qq.com>
2017-02-14 16:22:43 +0800

    配置中心注册为服务

--- liubo-output/src/main/java/com/liubo/output/LiuboOutputApplication.java
+++ liubo-output/src/main/java/com/liubo/output/LiuboOutputApplication.java
@@ -2,10 +2,12 @@ package com.liubo.output;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LiuboOutputApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(LiuboOutputApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LiuboOutputApplication.class, args);
+    }
 }
